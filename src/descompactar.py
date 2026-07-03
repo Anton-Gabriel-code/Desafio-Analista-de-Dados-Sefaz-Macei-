867e2eb73cf7cf24914646a59bb6b39c46ef26e6
Antonio Gabriel dos Santos Barbosa <antoniothecode@gmail.com>
2026-07-02 23:43:19 -0300

    Implementando algoritmo de descompactação e leitura de arquivos

--- src/descompactar.py
+++ src/descompactar.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import zipfile
+from pathlib import Path
 
+pasta_dados = Path('dados_compactos')
+
+for arquivo_zip in pasta_dados.glob('**/*.zip'):
+    print(f'Analisando arquivo: {arquivo_zip}')
+    
+    with zipfile.ZipFile(arquivo_zip, 'r') as z:
+        for nome_arquivo in z.namelist():
+            if nome_arquivo.endswith('.csv'):
+                tabela = pd.read_csv(z.open(nome_arquivo), sep = ';', encoding= 'latin-1', skiprows= 3, on_bad_lines= 'skip')
+                print(tabela.head())
